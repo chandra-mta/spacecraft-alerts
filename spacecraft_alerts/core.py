406d3c16cdc49011cc109e78dcccefdd97d4ace1
william-aaron-CFA <william.aaron@cfa.harvard.edu>
2025-10-24 16:58:56 -0400

    Module for package variables.

---
+++ spacecraft_alerts/core.py
@@ -0,0 +1,18 @@
+# Licensed under a 3-clause BSD style license - see LICENSE
+"""
+Core Variables and Functions
+"""
+
+CATEGORIES = [
+    "ACA",
+    "CCDM",
+    "EPHIN",
+    "EPS",
+    "PCAD",
+    "IRU",
+    "SIM-OTG",
+    "SI",
+    "TEL",
+    "EPS-SFMT",
+    "NORM-SFMT",
+]
